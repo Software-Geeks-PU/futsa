edeb8d47dff4e4eb350920a37cf13adeb6874d71
sarojluffy <sarojreus10@gmail.com>
2021-04-05 22:19:50 +0545

    changes

---
+++ index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='./index.css' type=text/css rel="stylesheet"/>
+    <title>Futsal Booking System</title>
+</head>
+<body>
+    <!--navigation bar  -->
+    <header>
+        <div class="nav-bar">
+          <div class="logo"><h2>FUTSA</h2></div> 
+          <div class = 'left-header'>
+          <h2>Login</h2>
+          <h2>Register</h2>
+        </div> 
+        </div> 
+    </header>
+
+    <section>
+        <div class="first-section">
+            <h2>FUTSA-Get ready to roll</h2>
+           <button id="w">Hello</button>
+          
+        </div>
+        <div class="nav-bar">
+            <h3>random text</h3>
+        </div>
+    </section>
+
+</body>
+</html>
