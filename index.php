6982728146a7f4db83d8fdf379ff33aafcd46ae7
joerush18 <joras.aryal23@gmail.com>
2021-04-06 18:28:43 +0545

    folderstructure for client view added

--- index.php
+++ index.php
@@ -1,39 +1,48 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='./index.css' type=text/css rel="stylesheet"/>
-    <title>Futsal Booking System</title>
+    <link href='./index.css' type=text/css rel="stylesheet" />
+    <title>FUTSA</title>
 </head>
+
 <body>
     <!--navigation bar  -->
     <header>
         <div class="nav-bar">
-          <div class="logo"><h2>FUTSA</h2></div> 
-          <div class = 'left-header'>
-          <h2>Login</h2>
-          <h2>Register</h2>
-        </div> 
-        </div> 
+            <div class="logo">
+                <h2>FUTSA</h2>
+            </div>
+            <div class="left-header">
+                <a href="./login/loginView.php">
+                    <h2>Login</h2>
+                </a>
+                <a href="./register/registerView.php">
+                    <h2>Register</h2>
+                </a>
+            </div>
+        </div>
     </header>
 
     <section>
         <div class="first-section">
             <h2>FUTSA-Get ready to roll</h2>
-           <button id="w">Hello</button>
-          
+            <button id="w">Hello</button>
+
         </div>
         <div class="nav-bar">
             <h3>random text</h3>
         </div>
     </section>
-    <section class="second-section" >
+    <section class="second-section">
         <div class="images"></div>
         <div class="images"></div>
         <div class="images"></div>
-   
+
     </section>
 
 </body>
+
 </html>
