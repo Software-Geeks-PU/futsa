477382350cec6d3f7c2af65e2e8368fd78de3a79
sarojluffy <sarojreus10@gmail.com>
2021-04-09 17:07:55 +0545

    form with username & password added

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='./index.css' type=text/css rel="stylesheet" />
+    <link href='templates/index.css' type=text/css rel="stylesheet" />
     <title>FUTSA</title>
 </head>
 
@@ -16,10 +16,10 @@
                 <h2>FUTSA</h2>
             </div>
             <div class="left-header">
-                <a href="./login/userLoginView.php">
+                <a href="../login/userLoginView.php">
                     <h2>Login</h2>
                 </a>
-                <a href="./register/userRegisterView.php">
+                <a href="../register/userRegisterView.php">
                     <h2>Register</h2>
                 </a>
             </div>
