2a25dc6df68872256ab18407718816ecbec015f3
Sandesh Thapa <uchihazoro02@gmail.com>
2021-04-09 17:07:54 +0545

    Register and fixed css

---
+++ register/userRegisterView.php
@@ -0,0 +1,22 @@
+
+<link href='../templates/index.css' type=text/css rel="stylesheet"/>
+<?php 
+    include '../templates/header.php';
+?>
+
+<h1>someting</h1>
+<form action="">
+NAME:<input type="text" value="Name">  <br><br>
+EMAIL:<input type="text"> <br><br>
+PASSWORD<input type="text"><br><br>
+ADDRESS:<input type="text"> <br><br>
+PHONE:<input type="text"> <br><br>
+<input type="checkbox"> Agree to all terms and condition<br><br>
+<input type="button" value="Button">
+<p><a href="#">Something</a></p>
+
+</form>
+
+<?php 
+    include '../templates/footer.php';
+?>
