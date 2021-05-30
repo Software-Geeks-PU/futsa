dd5d9aff3ab8c8f3ee263e62ff8801ed6c263713
kusaljr <prastutlamsal41@gmail.com>
2021-05-30 15:43:36 +0545

    database

--- futsa2.sql
+++ futsa2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2021 at 10:58 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.5
+-- Generation Time: May 30, 2021 at 11:58 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,7 +42,7 @@ CREATE TABLE `booking` (
 
 INSERT INTO `booking` (`booking_id`, `futsal_id`, `booker_id`, `book_time_id`, `status`, `date`) VALUES
 (1, 8, 1, 2, 'expired', '2021-05-29'),
-(2, 8, 1, 2, 'booked', '2021-05-30');
+(2, 8, 1, 2, 'expired', '2021-05-30');
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,25 @@ INSERT INTO `futsal` (`futsal_id`, `futsal_name`, `owner`, `address`, `phone_no`
 (5, 'Airport sport centre', 'Jontybhai', 'Chathey', '984666668', 'images/ppx.jfif'),
 (6, 'Ranipauwa Sports Academy', 'Jontybhai', 'Ranipauwa', '984666666', 'images/abc.jpg'),
 (7, 'Pokhara Futsal Centre', 'Jontybhai', 'Malepatan', '984666666', 'images/ppx.jfif'),
-(8, 'Burlakoti House', 'Sirish Burlakoti', 'Jhapa', '9861886847', 'https://anilblon.files.wordpress.com/2015/08/futsal-in-nepal.jpg');
+(8, 'Burlakoti House', 'Sirish Burlakoti', 'Jhapa', '9861886847', 'https://anilblon.files.wordpress.com/2015/08/futsal-in-nepal.jpg'),
+(9, '', 'Burlakoti ', 'Rambazar, Pokhara', '1234567890', 'https://anilblon.files.wordpress.com/2015/08/futsal-in-nepal.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owner_pending`
+--
+
+CREATE TABLE `owner_pending` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone_no` varchar(50) NOT NULL,
+  `futsal_name` varchar(100) NOT NULL,
+  `image` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -115,7 +133,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `address`, `phone_no`, `role`) VALUES
 (1, 'kusal lamsal', 'journeytous2024@gmail.com', '202cb962ac59075b964b07152d234b70', 'pokhara', '9861886847', 'user'),
-(2, 'Sirish Burlakoti', 'leosirish@gmail.com', '202cb962ac59075b964b07152d234b70', 'Jhapa', '9861886847', 'owner');
+(2, 'Sirish Burlakoti', 'leosirish@gmail.com', '202cb962ac59075b964b07152d234b70', 'Jhapa', '9861886847', 'owner'),
+(3, 'KusalJr', 'kusaljr@gmail.com', '202cb962ac59075b964b07152d234b70', 'Rambazar, Pokhara', '1234567890', 'admin'),
+(4, 'Burlakoti ', 'boula@gmail.com', 'd9b1d7db4cd6e70935368a1efb10e377', 'Rambazar, Pokhara', '1234567890', 'owner');
 
 --
 -- Indexes for dumped tables
@@ -143,6 +163,12 @@ ALTER TABLE `futsal`
   ADD PRIMARY KEY (`futsal_id`);
 
 --
+-- Indexes for table `owner_pending`
+--
+ALTER TABLE `owner_pending`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -168,13 +194,19 @@ ALTER TABLE `book_time`
 -- AUTO_INCREMENT for table `futsal`
 --
 ALTER TABLE `futsal`
-  MODIFY `futsal_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `futsal_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `owner_pending`
+--
+ALTER TABLE `owner_pending`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
